a17882ee54240724b35466b29d358beac1dc54b2
luftmyszor <tciba552@gmail.com>
2026-05-19 00:11:49 +0200

    delegated fstream management fully to DataManager

--- src/Repository.h
+++ src/Repository.h
@@ -1,10 +1,10 @@
 #pragma once
-// Repository.h
-// Purpose: generic in-memory container with load/save helpers for simple persistence.
 #include <bits/stdc++.h>
+#include "DataManager.h"
 
 template <typename T>
-class Repository {
+class Repository
+{
 private:
     std::vector<T> items;
 
@@ -14,32 +14,42 @@ public:
 
     void add(const T &item) { items.push_back(item); }
 
-    typename std::vector<T>::iterator find_if(std::function<bool(const T &)> pred) {
+    typename std::vector<T>::iterator find_if(std::function<bool(const T &)> pred)
+    {
         return std::find_if(items.begin(), items.end(), pred);
     }
 
-    void remove_if(std::function<bool(const T &)> pred) {
+    void remove_if(std::function<bool(const T &)> pred)
+    {
         items.erase(std::remove_if(items.begin(), items.end(), pred), items.end());
     }
 
     std::vector<T> &getAll() { return items; }
 
-    void sortBy(std::function<bool(const T &, const T &)> cmp) { std::sort(items.begin(), items.end(), cmp); }
+    void sortBy(std::function<bool(const T &, const T &)> cmp)
+    {
+        std::sort(items.begin(), items.end(), cmp);
+    }
 
-    void loadFromFile(const std::string &filename, std::function<T(const std::string &)> parser) {
-        std::ifstream ifs(filename);
-        if (!ifs) throw std::runtime_error("Cannot open file " + filename);
-        std::string line;
-        while (std::getline(ifs, line)) {
-            if (line.empty()) continue;
-            items.push_back(parser(line));
+    void loadFromFile(const std::string &filename, std::function<T(const std::string &)> parser)
+    {
+        auto lines = DataManager::readFromFile(filename);
+        for (const auto &line : lines)
+        {
+            if (!line.empty())
+            {
+                items.push_back(parser(line));
+            }
         }
     }
 
-    void saveToFile(const std::string &filename, std::function<std::string(const T &)> serializer) const {
-        std::ofstream ofs(filename);
-        if (!ofs) throw std::runtime_error("Cannot open file " + filename);
+    void saveToFile(const std::string &filename, std::function<std::string(const T &)> serializer) const
+    {
+        std::vector<std::string> lines;
         for (const T &it : items)
-            ofs << serializer(it) << "\n";
+        {
+            lines.push_back(serializer(it));
+        }
+        DataManager::saveToFile(filename, lines);
     }
 };
